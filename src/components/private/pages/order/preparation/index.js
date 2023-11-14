378a33b90294eb36c9d2a1e642d55b872debd888
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-11-14 23:28:45 +0530

    updated prepration

--- src/components/private/pages/order/preparation/index.js
+++ src/components/private/pages/order/preparation/index.js
@@ -26,34 +26,49 @@ const Preparation = (props) => {
   useEffect(() => {
     document.title = `Today Order - Diet Food Management Portal`;
   }, []);
+
   const [showAllReplacable, setShowAllReplacable] = useState(false);
   const [filterView, setFilterView] = useState({
     date: new Date().toISOString(),
     mealTimeCategory: "",
     typeOfRecipe: "",
   });
-  const [openRecipe, setOpenRecipe] = useState({});
+  const [openRecipe, setOpenRecipe] = useState([]);
   const [preparing, setPreparing] = useState([]);
   const [prepared, setPrepared] = useState([]);
+
   const takeData = () => {
-    // Fetch all recipe schedules here
-    getData({}, "preperation")
+    const filters = {
+      date: filterView.date,
+      mealTimeCategory: filterView.mealTimeCategory,
+      typeOfRecipe: filterView.typeOfRecipe,
+    };
+
+    getData(filters, "preperation")
+      .then((data) => {
+        console.log(data.data);
+        setPreparing(data.data.response);
+        setOpenRecipe(data.data.response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    getData(filters, "preperation/get-prepared")
       .then((data) => {
         console.log(data);
-        
-        const userName = data.data.response
-        console.log(userName);
-        console.log(data.data.response.user);
-        setPreparing(data.data.response);
-        setPrepared(data.prepared);
+        setPrepared(data.data.response);
+        setOpenRecipe(data.data.response);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+
   useEffect(() => {
     takeData();
-  }, []);
+  }, [filterView]);
+
   const [date] = useState({
     type: "date",
     placeholder: "Date",
@@ -68,6 +83,7 @@ const Preparation = (props) => {
     add: true,
     update: true,
   });
+
   const [mealtimeCategories] = useState({
     type: "select",
     apiType: "API",
@@ -86,6 +102,7 @@ const Preparation = (props) => {
     update: true,
     filter: true,
   });
+
   const [typeOfRecipe] = useState({
     type: "select",
     placeholder: "Type Of Recipe",
@@ -116,43 +133,28 @@ const Preparation = (props) => {
     };
     setFilterView(updateValue);
 
-    // Make an API call to fetch data based on the selected filters
-    getData({ mealTimeCategory: updateValue.mealTimeCategory }, "preperation")
+    getData(updateValue, "preperation")
       .then((data) => {
-        // Update your state with the retrieved data
+        console.log(data.data);
         setPreparing(data.data.response);
-        setPrepared(data.prepared);
+        setOpenRecipe(data.data.response);
       })
       .catch((error) => {
-        // Handle error
+        console.error(error);
+      });
+
+    getData(updateValue, "preperation/get-prepared")
+      .then((data) => {
+        console.log(data.data);
+        setPrepared(data.data.response);
+        setOpenRecipe(data.data.response);
+      })
+      .catch((error) => {
         console.error(error);
       });
   };
 
-  const moveRecipeToPrepared = (recipeId) => {
-    // Make an API call to update the status of the recipe to "prepared"
-    // Replace 'yourApiCall' with your actual API endpoint to update the status
-    getData({ recipeId }, "preperation/prepared")
-      .then((data) => {
-        // Handle success, e.g., refresh the list of recipes
-        // You might want to refetch the data or update the state accordingly
-        console.log("Recipe moved to Prepared", data);
-
-        // You can also update the state to reflect the change
-        // For example, you can remove the recipe from 'preparing' and add it to 'prepared'
-        // Make sure to update the state based on the response from your API call
-        setPreparing((prevPreparing) =>
-          prevPreparing.filter((recipe) => recipe._id !== recipeId)
-        );
-        setPrepared(data.preparedRecipes); // Update prepared recipes from the API response
-
-        // Handle other actions if needed
-      })
-      .catch((error) => {
-        // Handle error
-        console.error("Error moving recipe to Prepared", error);
-      });
-  };
+  console.log(preparing);
 
   return (
     <Container className="noshadow">
@@ -219,83 +221,95 @@ const Preparation = (props) => {
               <ColumnContainer className="gap">
                 <Items>
                   {preparing?.map((recipeItem, recepeIndex) => (
-                    <Recepe
-                      className={"recipe order"}
-                      key={`recipe-${recepeIndex}`}
-                    >
-                      <RecepeContent className="recipe1">
-                        <RecepeImage
-                          src={
-                            recipeItem.recipe.photo
-                              ? process.env.REACT_APP_CDN +
-                                recipeItem.recipe.photo
-                              : food
-                          }
-                        ></RecepeImage>
-                        <RecepeData className="recipe">
-                          <span className="title">
-                            {recipeItem.recipe.title}
-                          </span>
-                          <span className="light">
-                            <span>
-                              {recipeItem.recipe.gram?.toFixed(2)} gram
-                            </span>
-                            <span>
-                              {recipeItem.recipe.quantiy?.toFixed(0)} nos
-                            </span>
-                          </span>
-                          <div className="actions">
-                            <span
-                              className="delete full"
-                              title="View Recipe Info"
-                              onClick={() => {
-                                moveRecipeToPrepared(recipeItem._id);
-                              }}
-                            >
-                              Move to Prepared
-                              <GetIcon icon={"next"} />
-                            </span>
-                            <span
-                              className="info"
-                              title="View Recipe Info"
-                              onClick={() => {
-                                setOpenRecipe((prev) => ({
-                                  ...prev,
-                                  ["preparing_" + recipeItem._id]: !(
-                                    prev["preparing_" + recipeItem._id] ?? false
-                                  ),
-                                }));
-                              }}
-                            >
-                              {!(openRecipe[recipeItem._id] ?? false) ? (
-                                <GetIcon icon={"down"} />
-                              ) : (
-                                <GetIcon icon={"up"} />
-                              )}
-                            </span>
-                          </div>
-                          {openRecipe[recipeItem._id] && (
-                            <Patients>
-                              {/* {recipeItem.user?.map((item) => ( */}
-                                <Patient>
-                                  <span className="light">
-                                    <span className="bold">
-                                      {recipeItem.user.username}
-                                    </span>
-                                    <span className="bold">
-                                      {recipeItem.recipe.gram?.toFixed(2)} g
-                                    </span>
-                                    <span>{recipeItem.recipeNote}</span>
+                    <div key={`recipe-group-${recepeIndex}`}>
+                      {Array.isArray(recipeItem.recipe) ? (
+                        recipeItem.recipe.map((recipe, recipeIndex) => (
+                          <Recepe
+                            className={"recipe order"}
+                            key={`recipe-${recepeIndex}-${recipeIndex}`}
+                          >
+                            <RecepeContent className="recipe1">
+                              <RecepeImage
+                                src={
+                                  recipe.photo
+                                    ? process.env.REACT_APP_CDN + recipe.photo
+                                    : food
+                                }
+                              ></RecepeImage>
+                              <RecepeData className="recipe">
+                                <span className="title">{recipe.title}</span>
+                                <span className="light">
+                                  <span>{recipe.gram?.toFixed(2)} gram</span>
+                                  <span>{recipe.quantiy?.toFixed(0)} nos</span>
+                                </span>
+                                <div className="actions">
+                                  <span
+                                    className="delete full"
+                                    title="View Recipe Info"
+                                    // onClick={() => {
+                                    //   moveRecipeToPrepared(
+                                    //     recipeItem.data._id,
+                                    //     recipeItem.mealTimeCategory._id
+                                    //   );
+                                    // }}
+                                  >
+                                    Move to Prepared
+                                    <GetIcon icon={"next"} />
                                   </span>
-                                </Patient>
-                              {/* ))} */}
-                            </Patients>
-                          )}
-                        </RecepeData>
-                      </RecepeContent>
-                    </Recepe>
+                                  <span
+                                    className="info"
+                                    title="View Recipe Info"
+                                    onClick={() => {
+                                      setOpenRecipe((prev) => ({
+                                        ...prev,
+                                        [recipe._id]: !(
+                                          prev[recipe._id] ??
+                                          false
+                                        ),
+                                        // ["preparing_" + recipe._id]: !(
+                                        //   prev["preparing_" + recipe._id] ??
+                                        //   false
+                                        // ),
+                                      }));
+                                    }}
+                                  >
+                                    {!(openRecipe[recipe._id] ?? false) ? (
+                                      <GetIcon icon={"down"} />
+                                    ) : (
+                                      <GetIcon icon={"up"} />
+                                    )}
+                                  </span>
+                                </div>
+                                {openRecipe[recipe._id] && (
+                                  <Patients>
+                                    {recipeItem.users?.map((item) => (
+                                      item?.map((user) => (
+                                      <Patient key={`patient-${item.userId}`}>
+                                        <span className="light">
+                                          <span className="bold">
+                                            {user.username}
+                                          </span>
+                                          <span className="bold">
+                                            {recipe.gram?.toFixed(2)} g
+                                          </span>
+                                          <span>{recipeItem.recipeNote}</span>
+                                        </span>
+                                      </Patient>
+                                      ))
+                                    ))}
+                                  </Patients>
+                                )}
+                              </RecepeData>
+                            </RecepeContent>
+                          </Recepe>
+                        ))
+                      ) : (
+                        <p>Recipes array is not defined or is not an array</p>
+                      )}
+                    </div>
                   ))}
                 </Items>
+
                 <Items>
                   {prepared?.map((recipeItem, recepeIndex) => (
                     <Recepe
@@ -316,8 +330,12 @@ const Preparation = (props) => {
                             {recipeItem.recipe.title}
                           </span>
                           <span className="light">
-                            <span>{recipeItem.gram?.toFixed(2)} gram</span>
-                            <span>{recipeItem.quantiy?.toFixed(2)} nos</span>
+                            <span>
+                              {recipeItem.recipe.gram?.toFixed(2)} gram
+                            </span>
+                            <span>
+                              {recipeItem.recipe.quantiy?.toFixed(2)} nos
+                            </span>
                           </span>
                         </RecepeData>
                       </RecepeContent>
@@ -332,4 +350,5 @@ const Preparation = (props) => {
     </Container>
   );
 };
+
 export default Layout(Preparation);
