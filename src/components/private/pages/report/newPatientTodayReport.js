2e369dbb51f53e5d9bc571d0d31a0ed3e830e15b
Asharudheen-code <asharuashkar@gmail.com>
2023-11-27 18:19:21 +0530

    __

--- src/components/private/pages/report/newPatientTodayReport.js
+++ src/components/private/pages/report/newPatientTodayReport.js
@@ -4,99 +4,99 @@ import ListTable from "../../../elements/list/list";
 import { Container } from "../../common/layout/styels";
 
 const TodayPatientReport = (props) => {
-    useEffect(() => {
-        document.title = `Today Patient Report - Diet Food Management Portal`;
-    }, []);
+  useEffect(() => {
+    document.title = `Today Patient Report - Diet Food Management Portal`;
+  }, []);
 
-    // const themeColors = useSelector((state) => state.themeColors);
+  // const themeColors = useSelector((state) => state.themeColors);
 
-    const [attributes] = useState([
-        {
-            type: "text",
-            placeholder: "CPR Number",
-            name: "cprNumber",
-            showItem: "",
-            tag: false,
-            validation: "",
-            default: "",
-            label: "CPR Number",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "Name",
-            name: "userDisplayName",
-            showItem: "",
-            tag: false,
-            validation: "",
-            default: "",
-            label: "Name",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "Mobile Number",
-            name: "mobileNumber",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Mobile Number",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "E-Mail",
-            name: "emailId",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "E-Mail",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "date",
-            placeholder: "Reg Date",
-            name: "createdAt",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Reg Date",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-    ]);
+  const [attributes] = useState([
+    {
+      type: "text",
+      placeholder: "CPR Number",
+      name: "cprNumber",
+      showItem: "",
+      tag: false,
+      validation: "",
+      default: "",
+      label: "CPR Number",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "Name",
+      name: "userDisplayName",
+      showItem: "",
+      tag: false,
+      validation: "",
+      default: "",
+      label: "Name",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "Mobile Number",
+      name: "mobileNumber",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Mobile Number",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "E-Mail",
+      name: "emailId",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "E-Mail",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "date",
+      placeholder: "Reg Date",
+      name: "createdAt",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Reg Date",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+  ]);
 
-    return (
-        <Container className="noshadow">
-            {/* Render a ListTable component */}
-            <ListTable
-                // actions={actions}
-                api={`user/subscriber/today-created`}
-                itemTitle={{ name: "cprNumber", type: "text", collection: "" }}
-                // profileImage="photo"
-                shortName={`Today Patient Report`}
-                formMode={`double`}
-                viewMode="table"
-                {...props}
-                attributes={attributes}
-                datefilter={true}
-            ></ListTable>
-        </Container>
-    );
+  return (
+    <Container className="noshadow">
+      {/* Render a ListTable component */}
+      <ListTable
+        // actions={actions}
+        api={`user/subscriber/today-created`}
+        itemTitle={{ name: "cprNumber", type: "text", collection: "" }}
+        // profileImage="photo"
+        shortName={`Today Patient Report`}
+        formMode={`double`}
+        viewMode="table"
+        {...props}
+        attributes={attributes}
+        datefilter={true}
+      ></ListTable>
+    </Container>
+  );
 };
 
 export default Layout(TodayPatientReport);
